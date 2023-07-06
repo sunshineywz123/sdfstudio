1bfd6e6d98a9c29db9e1e8a411d80981cb04133d
sunshineywz <481056926@qq.com>
2023-07-06 21:00:03 +0800

    Create train images with masked out objects

---
+++ extract_mesh_texture.sh
@@ -0,0 +1,4 @@
+YAML = outputs/data-custom-pikaqiu_dazhanbi/neus-facto/2023-07-06_202056/config.yml
+PLY = meshes/pikaqiu_dazhanbi.ply
+ns-extract-mesh --load-config $YAML --output-path $PLY
+python scripts/texture.py --load-config $YAML --input-mesh-filename $PLY --output-dir ./textures_pikaqiu --target_num_faces 50000
